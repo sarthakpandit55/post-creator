d77e5b22a30b87b9b5020ddaf6560d12c0eb6dfe
sarthak <sarthakpandit55@gmail.com>
2025-08-03 23:48:57 +0530

    added feature of creating post

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const cookieParser = require('cookie-parser');
 const bcript = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { hash } = require('crypto');
+const user = require('./models/user');
 
 
 // middleware
 app.set('view engine', 'ejs');
 app.use(express.json());
-app.use(express.urlencoded({exptended : true}));
+app.use(express.urlencoded({ extended : true}));
 app.use(cookieParser());
 
 
@@ -26,10 +27,27 @@ app.get('/login', (req, res) => {
 })
 
 // profile
-app.get('/profile', isLoggedIn, (req, res) => {
-    console.log(req.user);
-    res.render('login');
+app.get('/profile', isLoggedIn, async (req, res) => {
+    let {email} = req.user.email;
+    let user = await userModel.findOne(email).populate("posts");
+    res.render('profile', {user});
+
 })
+
+// post creating
+
+app.post('/post', isLoggedIn, async (req, res) => {
+    let user = await userModel.findOne({email : req.user.email})
+    let {content} = req.body
+    let post = await postModel.create({
+        user : user._id,
+        content : content
+    });
+
+    user.posts.push(post._id);
+    await user.save();
+    res.redirect('/profile');
+});
 
 // creating new user
 app.post('/register', async (req,res) => {
@@ -63,7 +81,7 @@ app.post('/login', async (req,res) => {
         if(result){
             let token = jwt.sign({email : email, userid : user._id}, "shhhh");
             res.cookie("token", token );
-            res.status(200).send("you can login");
+            res.status(200).redirect('/profile');
         }
         else{
             res.render('login');
@@ -80,7 +98,7 @@ app.get('/logout', (req, res) => {
 // middleware to protect
 function isLoggedIn(req, res, next){
     if(req.cookies.token === ""){
-        res.send("you must login first");
+        res.redirect('/login');
     }
     else{
         let data = jwt.verify(req.cookies.token, "shhhh");
